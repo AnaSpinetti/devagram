f84a85f671460d25c533d263102496d0097564e2
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-06-17 18:01:39 -0300

    api de busca concluida

--- pages/api/busca.ts
+++ pages/api/busca.ts
@@ -7,24 +7,36 @@ import { UsuarioModel } from "../../models/UsuarioModel";
 export const buscaEndPoint = async (req: NextApiRequest, res: NextApiResponse<PadraoResponse | any[]>) => {
     try {
         if (req.method === 'GET') {
-            const { filtro } = req.query;
+            if (req?.query?.id) {
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id)
 
-            if (!filtro || filtro.length < 2) {
-                return res.status(405).json({ error: 'Informe pelo menos 2 caracteres para a busca' })
+                if(!usuarioEncontrado){
+                    return res.status(400).json({ error: 'Nenhum usuário localizado' })
+                }
+
+                usuarioEncontrado.senha=null;
+                return res.status(200).json(usuarioEncontrado);
+
+            } else {
+
+                const { filtro } = req.query;
+
+                if (!filtro || filtro.length < 2) {
+                    return res.status(405).json({ error: 'Informe pelo menos 2 caracteres para a busca' })
+                }
+
+                // Realizando a busca sem ser pelo valor exato 
+                const usuarioslocalizados = await UsuarioModel.find({
+                    $or: [{ nome: { $regex: filtro, $options: 'i' } },
+                    { email: { $regex: filtro, $options: 'i' } }]
+                })
+
+                if (!usuarioslocalizados) {
+                    return res.status(400).json({ error: 'Nenhum usuário localizado' })
+                }
+
+                return res.status(200).json(usuarioslocalizados);
             }
-
-            // Realizando a busca sem ser pelo valor exato 
-            const usuarioslocalizados = await UsuarioModel.find({
-                $or: [{ nome: { $regex: filtro, $options: 'i' }}, 
-                { email: { $regex: filtro, $options: 'i' } }]
-            })
-
-            if (!usuarioslocalizados) {
-                return res.status(400).json({ error: 'Nenhum usuário localizado' })
-            }
-
-            return res.status(200).json(usuarioslocalizados);
-
         }
 
         return res.status(405).json({ error: 'O método informado é inválido' })
