233c672f9d5b422563bec7033fcaf8a7197dffea
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-06-19 20:49:26 -0300

    Feed home finalizado

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,26 +4,44 @@ import { ValidarJWT } from "../../middlewares/ValidarJWT";
 import { ConectarMongoDb } from "../../middlewares/ConectarMongoDb";
 import { UsuarioModel } from "../../models/UsuarioModel";
 import { PublicacaoModel } from "../../models/PublicacaoModel";
+import { SeguidorModel } from "../../models/SeguidorModel";
 
 const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<PadraoResponse | any>) => {
     try {
         if(req.method === "GET"){
         // Buscando o usuario logado
-        if(req?.query?.id){
-            const usuario = await UsuarioModel.findById(req?.query?.id);
+            if(req?.query?.id){
+                const usuario = await UsuarioModel.findById(req?.query?.id);
 
-            if(!usuario){
-                return res.status(400).json({error: 'usuário não localizado'})
+                if(!usuario){
+                    return res.status(400).json({error: 'usuário não localizado'})
+                }
+
+                const publicacoes = await PublicacaoModel.find({idUsuario: usuario._id}).sort({data: -1})
+
+                return res.status(200).json(publicacoes);
+            }else{
+                const {userId} = req.query
+                const usuarioLogado = await UsuarioModel.findById(userId);
+
+                if(!usuarioLogado){
+                    return res.status(400).json({error: 'usuário não localizado'})
+                }
+
+                const seguidores = await SeguidorModel.find({_id: usuarioLogado._id})
+                const seguidoresIds = seguidores.map(e => e.idUsuarioSeguido)
+                const publicacoes = await PublicacaoModel.find({
+                    $or: [
+                        { idUsuario: usuarioLogado._id },
+                        {idUsuario: seguidoresIds}
+                    ]    
+                }).sort({data: -1})
+
+
+                return res.status(200).json(publicacoes)
             }
-
-            const publicacoes = await PublicacaoModel.find({idUsuario: usuario._id}).sort({data: -1})
-
-            return res.status(200).json(publicacoes);
         }
-
-        }else{
-            return res.status(405).json({error: 'O método informado é inválido'})
-        }
+        return res.status(405).json({error: 'O método informado é inválido'})
 
     } catch (error) {
         console.log(error)
